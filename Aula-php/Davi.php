ca27f135e41058d928dc0b47215fa00363ca0be6
João Pedro De Menezes <jpm3nezes@gmail.com>
2026-05-20 19:25:14 -0300

    Add files via upload

---
+++ Aula-php/Davi.php
@@ -0,0 +1,12 @@
+<?php
+
+// IF
+$usuario = "joao";
+$idade = 14;
+
+if ($idade < 15 || $idade > 50){
+    echo "<h1>sua idade é $idade, não autorizado a entrada</h1>";
+}else{
+    echo "<h1>Bem vindo</h1>";
+}
+?>
